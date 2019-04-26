2c62944c390b6d450308d9899482b6febe80a749
WMXPY <wm@mengw.io>
2019-04-25 19:53:08 -0500

    add recursive

---
+++ src/config/recursive.ts
@@ -0,0 +1,56 @@
+/**
+ * @author WMXPY
+ * @namespace Read
+ * @description Recursive
+ */
+
+import { readTextFile } from "@sudoo/io";
+import { _Json } from "@sudoo/bark/json";
+import { _Map } from "@sudoo/bark/map";
+
+const isObject = (element: any): element is Object => (element !== null && typeof element === 'object');
+
+export const recursiveRead = async (path: string, depth: number = 0, maxDepth: number = 15): Promise<any> => {
+
+    const content: string = await readTextFile(path);
+    const parsed: any = _Json.safeParse(content, new Error('failed'));
+
+    return await recursiveParse(parsed, depth + 1, maxDepth);
+};
+
+export const recursiveParse = async (parsed: any, depth: number = 0, maxDepth: number = 15): Promise<any> => {
+
+    if (Array.isArray(parsed)) {
+
+        return parsed.map(async (element: any) => {
+            if (typeof element === 'string' && element.match(/$\$ref:.+/)) {
+                const targetPath: string = element.replace('$ref:', '');
+                const replacement: Array<any> = await recursiveRead(targetPath);
+
+                if (Array.isArray(replacement)) {
+                    return replacement;
+                }
+            }
+            return element;
+        })
+    } else if (isObject(parsed)) {
+
+        const keys: string[] = _Map.keys(parsed);
+
+        return keys.reduce(async (previous: Record<any, any>, current: string) => {
+
+            if (current === '$ref') {
+
+                const replacement: Record<any, any> = await recursiveRead(parsed[current]);
+                if (isObject(replacement)) {
+                    return { ...previous, ...replacement };
+                }
+                throw new Error('not object');
+            }
+            return {
+                ...previous,
+                [current]: parent[current],
+            };
+        }, {} as Record<any, any>);
+    }
+};
