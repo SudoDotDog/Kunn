8ff98d9cc823bdd6b67b6fe7396f1ee81c5b9235
WMXPY <wm@mengw.io>
2019-04-26 20:55:05 -0500

    consume io functions

--- src/config/read.ts
+++ src/config/read.ts
@@ -4,9 +4,9 @@
  * @description Read
  */
 
+import { recursiveRead } from "@sudoo/io";
 import { KunnConfig } from "../declare/kunn";
 import { ERROR_CODE, panic } from "../panic/panic";
-import { recursiveRead } from "./recursive";
 import { validateKunnConfig } from "./validate";
 
 export const readConfig = async (path: string): Promise<KunnConfig> => {
