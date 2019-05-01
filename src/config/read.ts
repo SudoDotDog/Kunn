90fc2c3df94169b8fe1624a49405451d6a3e3004
WMXPY <wm@mengw.io>
2019-05-01 00:13:08 -0500

    consume kunn core

--- src/config/read.ts
+++ src/config/read.ts
@@ -6,12 +6,17 @@
 
 import { Kunn, KunnConfig } from "@kunn/core";
 import { recursiveRead } from "@sudoo/io";
+import { ERROR_CODE, panic } from "../panic/panic";
 
 export const fromConfig = async (path: string): Promise<Kunn> => {
 
     const parsed: KunnConfig = await recursiveRead(path);
 
     const kunn: Kunn = Kunn.fromConfig(parsed);
+
+    if (!kunn.validate()) {
+        throw panic.code(ERROR_CODE.CONFIG_NOT_VALID);
+    }
 
     return kunn;
 };
